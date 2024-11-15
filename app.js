082b416566b0d2f68bfd2464e48fd6d309b619ff
Bidyut Samanta <bidyutsamanta293@gmail.com>
2024-11-15 16:35:20 +0530

    how to create module and how to export module

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-function Hello () {
-    console.log('Hey Myself Bidyut. Initial setup done.!')
-}
+// function Hello () {
+//     console.log('Hey Myself Bidyut. Initial setup done.!')
+// }
+
+// Hello()
+
+var logger = require('./logger')
+
+logger.log('BIDYUT')
+// console.log(logger)
---
+++ logger.js
@@ -0,0 +1,7 @@
+const url = 'https://loggerjs.in'
+
+function log (message) {
+    console.log(`Hello ${message} Module is export like this`)
+}
+
+module.exports.log = log
