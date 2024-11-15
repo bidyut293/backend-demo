63a69567259c9027f747e02caf6fe30404c3b58a
Bidyut Samanta <bidyutsamanta293@gmail.com>
2024-11-15 17:16:46 +0530

    path module usecase

--- app.js
+++ app.js
@@ -4,7 +4,29 @@
 
 // Hello()
 
-var logger = require('./logger')
 
-logger.log('BIDYUT')
+
+
+//Custom module and exports that module
+// var logger = require('./logger')
+
+// logger.log('BIDYUT')
+// logger.check()
 // console.log(logger)
+
+
+// Path Module
+const path = require('path')
+
+var pathObj = path.parse(__filename)
+
+console.log('pathObj',pathObj)
+
+// output :
+// pathObj {
+//     root: '/',
+//     dir: '/Users/bidyutsamanta/Desktop/Backend/Nodejs/first-app',
+//     base: 'app.js',
+//     ext: '.js',
+//     name: 'app'
+//   }
--- logger.js
+++ logger.js
@@ -4,4 +4,9 @@ function log (message) {
     console.log(`Hello ${message} Module is export like this`)
 }
 
+const check = () => {
+    console.log('HelloBidyut')
+}
+
 module.exports.log = log
+module.exports.check = check
